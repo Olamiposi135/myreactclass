9bcb54c45fca84b8a71bcfd76ea83b58b728d1ff
Olamiposi135 <gbadamosiolamipo135@gmail.com>
2025-05-01 10:35:10 -0700

    update

---
+++ src/component/LeaderCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const LeaderCard = () => {
+  const leaderList = [
+    {
+      name: "Chidi Nwosu",
+      position: "Production Manager",
+      department: "Design",
+      image: "https://randomuser.me/api/portraits/men/32.jpg",
+    },
+    {
+      name: "Niniola Samuel",
+      position: "General Manager",
+      department: "Web Developer",
+      image: "https://randomuser.me/api/portraits/men/32.jpg",
+    },
+    {
+      name: "Kazeem Awoyay",
+      position: "Project Manager ",
+      department: "Social Advert",
+      image: "https://randomuser.me/api/portraits/men/32.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className=" text-3xl font-bold ">OUR LEADERS</h1>
+
+      <section className="flex  m-auto my-5 py-10">
+        {leaderList.map((leader) => (
+          <div className=" max-w-xs bg-white rounded-2xl shadow-md overflow-hidden m-4 ">
+            <img
+              src={leader.image}
+              alt="Pic"
+              className="w-ful h-48 object-cover"
+            />
+            <div className="p-4 ">
+              <h2 className="text-xl font-bold text-gray-800">{leader.name}</h2>
+              <p className="text-gray-600">
+                <span className="font-semibold"> Position: </span>
+                {leader.position}
+              </p>
+              <p className="text-gray-600">
+                <span className="font-semibold "> Department: </span>
+                {leader.department}
+              </p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default LeaderCard;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,69 @@
 import CardSection from "../component/CardSection";
 import Footer from "../component/Footer";
 import HeroSection from "../component/HeroSection";
+import LeaderCard from "../component/LeaderCard";
 
 import Navbar2 from "../component/Navbar2";
+import StaffCard from "../component/StaffCard";
 
 function Homepage() {
-  return ( 
+  return (
     <div>
       <Navbar2 />
       <HeroSection />
-      <CardSection />
+      <div className=" flex flex-col md:flex-row  max-w-[1140px] mx-auto  p-5  my-20 space-x-6 ">
+        <CardSection
+          image="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1019/LogoECSPublishingGroup1-removebg-preview-1-e1721125158123.png"
+          percent="10% list growth"
+          header="& sales spike in tough niche"
+          details="It's nice that with GetResponse, we have the tools and integrations we need within our budget."
+          ctaButton="Read case study"
+        />
+        <CardSection
+          image="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1022/living_vision-removebg-preview-e1721125329100.png"
+          percent="75% of all public sales"
+          header="from email autoresponders"
+          details="Together with GetResponses, we've been able to convince customers of
+            need, culminating in compelling sales."
+          ctaButton="Read case study"
+        />
+        <CardSection
+          image="https://us-wd.gr-cdn.com/customers/sites/16/2024/07/1025/red-hot-logo1_-removebg-preview-e1721125532901.png"
+          percent="Up to $1000"
+          header="for every 1000
+            emails sent"
+          details="Regarding revenue contribution, the email marketing channel is like
+            free money for us."
+          ctaButton="Read case study"
+        />
+      </div>
+
+      <div className="flex max-w-[1000px] mx-auto">
+        <StaffCard
+          name="Segun"
+          position="Manager"
+          department="Auditor"
+          image="https://randomuser.me/api/portraits/men/32.jpg"
+        />
+        <StaffCard
+          name="Adewale"
+          position="Clerk"
+          department="Human Resources"
+          image="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+        <StaffCard
+          name="Olamipo"
+          position="GM"
+          department="CEO"
+          image="https://randomuser.me/api/portraits/men/22.jpg"
+        />
+      </div>
+      <div className="flex w-full mx-auto">
+        <LeaderCard />
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
-
 
 export default Homepage;
